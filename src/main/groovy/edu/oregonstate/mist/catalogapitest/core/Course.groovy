221fcc4a9847a16bd51daa95f1cfaf0537959603
Brandon <brandonlee503@gmail.com>
2015-07-17 11:32:56 -0700

    Updated to support Groovy's implicit constructors, getters, and setters

--- src/main/groovy/edu/oregonstate/mist/catalogapitest/core/Course.groovy
+++ src/main/groovy/edu/oregonstate/mist/catalogapitest/core/Course.groovy
@@ -7,82 +7,11 @@ import javax.xml.bind.annotation.XmlRootElement
 @XmlRootElement
 class Course {
 
-    private Integer cid
-    private Integer crn
-    private String courseName
-    private String instructor
-    private String day
-    private String time
-    private String location
-
-    public Course() {
-        // Jackson Deserialization
-    }
-
-    // Getters
-    @JsonProperty
-    Integer getCid() {
-        return cid
-    }
-
-    @JsonProperty
-    Integer getCrn () {
-        return crn
-    }
-
-    @JsonProperty
-    String getCourseName() {
-        return courseName
-    }
-
-    @JsonProperty
-    String getInstructor() {
-        return instructor
-    }
-
-    @JsonProperty
-    String getDay() {
-        return day
-    }
-
-    @JsonProperty
-    String getTime() {
-        return time
-    }
-
-    @JsonProperty
-    String getLocation(){
-        return location
-    }
-
-    // Setters
-    @JsonProperty
-    void setCid(cid) {
-        this.cid = cid
-    }
-
-    @JsonProperty
-    void setCrn(crn) {
-        this.crn = crn
-    }
-
-    @JsonProperty
-    void setCourseName(courseName) {
-        this.courseName = courseName
-    }
-
-    @JsonProperty
-    void setInstructor(instructor) {
-        this.instructor = instructor
-    }
-
-    @JsonProperty
-    void setDay(day) {
-        this.day = day
-    }
-
-    @JsonProperty
-    void setTime(time) {
-        this.time = time
-    }
+    Integer cid
+    Integer crn
+    String courseName
+    String instructor
+    String day
+    String time
+    String location
 }
