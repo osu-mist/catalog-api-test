1a3e8b9af8dce3233ab0c5710abe99e31991faba
Brandon <brandonlee503@gmail.com>
2015-07-21 14:58:01 -0700

    Removed as much of properties as possible, still need constructors, but got rid of getters/setters

--- src/main/groovy/edu/oregonstate/mist/catalogapitest/core/ErrorPOJO.groovy
+++ src/main/groovy/edu/oregonstate/mist/catalogapitest/core/ErrorPOJO.groovy
@@ -1,7 +1,5 @@
 package edu.oregonstate.mist.catalogapitest.core
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 class ErrorPOJO {
     String errorMessage
     Integer errorCode
@@ -9,31 +7,9 @@ class ErrorPOJO {
     public ErrorPOJO() {
         // Jackson deserialization
     }
-}
-/*
+
     public ErrorPOJO(String errorMessage, Integer errorCode) {
         this.errorMessage = errorMessage
         this.errorCode = errorCode
     }
-
-    @JsonProperty
-    public String getErrorMessage() {
-        return errorMessage
-    }
-    @JsonProperty
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage
-    }
-
-    @JsonProperty
-    public Integer getErrorCode() {
-        return errorCode
-    }
-
-    @JsonProperty
-    public void setErrorCode(Integer errorCode) {
-        this.errorCode = errorCode
-    }
-
 }
-*/
