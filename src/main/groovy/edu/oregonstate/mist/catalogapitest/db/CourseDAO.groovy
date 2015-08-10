37b0bcd10d47f3fd54c69db2263dcfb80b9b0ae7
Brandon <brandonlee503@gmail.com>
2015-08-10 15:18:19 -0700

    Removed FIXME

--- src/main/groovy/edu/oregonstate/mist/catalogapitest/db/CourseDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/catalogapitest/db/CourseDAO.groovy
@@ -36,7 +36,6 @@ public interface CourseDAO extends Closeable {
     Course getByCrn(@Bind("crn") Integer crn)
 
     // Get specific instance number
-    // FIXME: possible race condition
     @SqlQuery("""
             SELECT COURSES_SEQ.CURRVAL FROM DUAL
             """)
