dcf9266625bde1280d281d8b32ef7efaa7d38515
Brandon <brandonlee503@gmail.com>
2015-09-08 11:13:29 -0700

    Removed unnecessary constructors (groovy named argument constructors)

--- src/main/groovy/edu/oregonstate/mist/catalogapitest/core/ErrorPOJO.groovy
+++ src/main/groovy/edu/oregonstate/mist/catalogapitest/core/ErrorPOJO.groovy
@@ -6,13 +6,4 @@ package edu.oregonstate.mist.catalogapitest.core
 class ErrorPOJO {
     String errorMessage
     Integer errorCode
-
-    public ErrorPOJO() {
-        // Jackson deserialization
-    }
-
-    public ErrorPOJO(String errorMessage, Integer errorCode) {
-        this.errorMessage = errorMessage
-        this.errorCode = errorCode
-    }
 }
